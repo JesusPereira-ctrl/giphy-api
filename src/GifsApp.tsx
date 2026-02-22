fc3fa782d20127f249ff9480d0148d02b7abd485
Jesus Pereira <jesuspereirap100@gmail.com>
2026-02-22 14:09:45 -0300

    first commit

---
+++ src/GifsApp.tsx
@@ -0,0 +1,34 @@
+import { GifList } from './gifs/components/GifList';
+import { PreviousSearches } from './gifs/components/PreviousSearches';
+import { useGifs } from './gifs/hooks/useGifs';
+import { CustomHeader } from './shared/components/CustomHeader';
+import { SearchBar } from './shared/components/SearchBar';
+
+export const GifsApp = () => {
+  const { gifs, previousTerms, handleSearch, handleTermClicked } = useGifs();
+
+  return (
+    <>
+      {/* Header */}
+      <CustomHeader
+        title='Buscador de Gifs'
+        description='Descubre y comparte el Gif perfecto'
+      />
+
+      {/* Search */}
+      <SearchBar
+        placeholder='Busca lo que quieras'
+        onQuery={handleSearch}
+      />
+
+      {/* Busquedas previas */}
+      <PreviousSearches
+        searches={previousTerms}
+        onLabelClicked={handleTermClicked}
+      />
+
+      {/* Gifs */}
+      <GifList gifs={gifs} />
+    </>
+  );
+};
